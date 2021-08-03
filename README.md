b5164cf2d05772e9f24179d897405aaf22252232
kizarrd <51534324+kizarrd@users.noreply.github.com>
2021-08-03 23:18:34 +0900

    getting to know more about matchlist api systems

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # lolapi
 lol stat website using lol api
 
-default main
+default branch main
 
 
 # app features(pages)
@@ -15,12 +15,23 @@ default main
     - items that worked best for my champions
     - the most common enemy that I met against my champions
 
-[] MVC settings
-[] fetch lolapi data and show it on view
-[] build routers
-[] design in detail
+[ ] MVC settings
+[ ] fetch lolapi data and show it on view
+[ ] build routers
+[ ] design in detail
 
+# ideas
+milliseconds to date gogo
 
 # Questions & Problems
 1. can I res.render an html file? (instead of using pug as view engine)
 2. when using fetch, summoner name is included in url. the problem is, if the name is in korean, error occurs. 
+
+
+# logs
+### 3rd Aug 2021. TUE
+ - the maximum matchlist range allowed for one query is 100
+    - e.g. if you played tf for 431 games, then you need to execute 5 queries in total to cover all 431 games. (0-100, 100-200, 200-300, 300-400, 400-432)
+    - lol api matchlist index range seems it doesn't count the endIndex.
+        - which means that if startIndex(beginIndex): 0 and endIndex: 100, then it actually shows the list from 0 to 99, excluding the 100. 
+        - so if you want a list of 100 matches from 0 to 99, then you need to input 0 for startIndex(beginIndex) and 100 for the endIndex. 
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,11 +1,12 @@
 import fetch from "node-fetch";
 
-const API_KEY = "RGAPI-5ac00e0d-d5b2-4898-a3c9-eaecf3f95ddf";
+const API_KEY = "RGAPI-1ee6e8f2-bbe6-4a33-ade9-0e992b4b2d20";
 const API_ROOT = "https://kr.api.riotgames.com/";
 const SUMMONERS_BY_NAME = "lol/summoner/v4/summoners/by-name/";
 const MATCHLISTS_BY_ACCOUNT = "lol/match/v4/matchlists/by-account/";
 const SUMMONER_NAME = "summit road";
 let SUMMONER_ID_ENCRYPTED = ""; 
+const championId = "4";
 
 export const home = async (req, res) => {
     // const accountData = await fetch(`${API_ROOT+SUMMONERS_BY_NAME+SUMMONER_NAME}?api_key=${API_KEY}`)
@@ -17,11 +18,18 @@ export const home = async (req, res) => {
         accountId: SUMMONER_ID_ENCRYPTED
     } = await (await fetch(`${API_ROOT+SUMMONERS_BY_NAME+SUMMONER_NAME}?api_key=${API_KEY}`)).json();
 
+    
     const matchlist = await (await fetch(`${API_ROOT+MATCHLISTS_BY_ACCOUNT+SUMMONER_ID_ENCRYPTED}?api_key=${API_KEY}`)).json();
+    
+    const matchlist_by_champion = await (await fetch(`${API_ROOT+MATCHLISTS_BY_ACCOUNT+SUMMONER_ID_ENCRYPTED}?champion=${championId}&api_key=${API_KEY}`)).json();
+    // console.log(matchlist);
+    console.log("match length: ", matchlist.matches.length);
+    console.log("total games: ", matchlist.totalGames);
+    // console.log(matchlist_by_champion);
+    console.log("list length: ", matchlist_by_champion.matches.length);
+    console.log("total games: ", matchlist_by_champion.totalGames);
 
-    console.log(matchlist);
-
-    console.log(matchlist.matches.length);
+    console.log(SUMMONER_ID_ENCRYPTED);
 
     res.render("home");
 };
