892e54129578311685d8f93557fbe55da58d8b2f
kizarrd <51534324+kizarrd@users.noreply.github.com>
2021-10-15 01:41:59 +0900

    riot verification

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -6,3 +6,7 @@ export const home = (req, res) => {
     }
     return res.render("home");
 };
+
+export const riotVerification = (req, res) => {
+    res.send("10cfc04f-9e97-496c-9ed8-d6b97cf93553");
+}
--- src/routers/homeRouter.js
+++ src/routers/homeRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { home } from "../controllers/homeController";
+import { home, riotVerification } from "../controllers/homeController";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
+rootRouter.get("/riot.txt", riotVerification);
 
 export default rootRouter;
