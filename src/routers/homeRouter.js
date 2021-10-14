aac58ceeec3829149badb64aab012236f7c72432
kizarrd <51534324+kizarrd@users.noreply.github.com>
2021-10-15 02:22:53 +0900

    riot text file

--- src/routers/homeRouter.js
+++ src/routers/homeRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { home, riotVerification } from "../controllers/homeController";
+import { home } from "../controllers/homeController";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
-rootRouter.get("/riot.txt", riotVerification);
+rootRouter.get("/riot.txt", (req, res) => {
+    res.sendFile(__dirname+"/riot.txt");
+});
 
 export default rootRouter;
