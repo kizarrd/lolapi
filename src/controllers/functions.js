b3de2a3e294ae2da3cd385b1e50a75eb0615398f
kizarrd <51534324+kizarrd@users.noreply.github.com>
2021-10-24 21:02:41 +0900

    update api key

--- src/controllers/functions.js
+++ src/controllers/functions.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 import ChampionRecord from "../models/ChampionRecord";
 import { championId_by_championName } from "./champion_processed";
 
-const API_KEY = "RGAPI-40770e28-17fa-4c5d-8e8c-a9a69089da92";
+const API_KEY = "RGAPI-ae934831-9f41-4e4e-8d2a-0719d9e54dfd";
 const API_ROOT = "https://kr.api.riotgames.com/lol/";
 const API_ROOT_ASIA = "https://asia.api.riotgames.com/lol/";
 const MATCH_BY_MATCHID = "match/v5/matches/";
